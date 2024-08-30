beb12558d27e1fd63bf0a20f366c460a9d6321f8
rechen-werk <adrian.vinojcic@gmail.com>
2024-08-31 00:13:25 +0200

    work on sidebar alignment for mobile and desktop

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,21 +1,11 @@
 import { Component } from '@angular/core';
-import {ControlService} from "../../services/controll-service/control.service";
-import {NgClass} from "@angular/common";
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [
-    NgClass
-  ],
+  imports: [],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
 export class SidebarComponent {
-  show!: boolean;
-  constructor(private controlService: ControlService) {
-    this.controlService.showSidebar$.subscribe(value => {
-      this.show = value;
-    });
-  }
 }
