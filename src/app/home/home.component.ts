ff6f48a79411a681484371dcdef3cd989fe23ba7
rechen-werk <adrian.vinojcic@gmail.com>
2024-08-31 19:26:16 +0200

    add footer, icons and adjust sidebar width

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,12 @@
 import {Component} from '@angular/core';
+import {FooterComponent} from "../footer/footer.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    FooterComponent
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
