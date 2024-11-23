4744fd6212ce1ec614a89d7f9327b44cc95da304
Rechenwerk <adrian.vinojcic@gmail.com>
2024-11-23 19:46:10 +0100

    remove sidebar, and refactor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import {NavbarComponent} from "./navbar/navbar.component";
-import {MainComponent} from "./main/main.component";
+import {HeaderComponent} from "./header/header.component";
+import {HomeComponent} from "./home/home.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, MainComponent],
+  imports: [HeaderComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/services/controll-service/control.service.ts
+++ src/services/controll-service/control.service.ts
@@ -1,17 +1,7 @@
 import { Injectable } from '@angular/core';
-import {BehaviorSubject} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ControlService {
-  private showSidebarSubject = new BehaviorSubject<boolean>(window.innerWidth >= 576);
-  showSidebar$ = this.showSidebarSubject.asObservable();
-
-  constructor() {
-
-  }
-  toggleSidebar() {
-    this.showSidebarSubject.next(!this.showSidebarSubject.value);
-  }
 }
