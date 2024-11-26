075c68bc0a3d78da195f8c15604864e0296f9075
rechenwerk <adrian@rechenwerk.eu>
2024-11-26 15:47:10 +0100

    put footer into app component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,12 @@
 import {Routes} from '@angular/router';
 import {BachelorThesisComponent} from "./routes/bachelor-thesis/bachelor-thesis.component";
 import {HomeComponent} from "./routes/home/home.component";
+import {NotFoundComponent} from "./routes/not-found/not-found.component";
+import {ContactComponent} from "./routes/contact/contact.component";
 
 export const routes: Routes = [
+  {path: 'contact', component: ContactComponent},
   {path: 'bachelor-thesis', component: BachelorThesisComponent},
   {path: '', component: HomeComponent},
-  {path: '**', redirectTo: ''},
+  {path: '**', component: NotFoundComponent},
 ];
