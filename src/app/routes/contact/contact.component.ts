1cb8da970449ca8b4cb28c27cb2d5c4bbfc7141d
rechenwerk <adrian@rechenwerk.eu>
2024-11-26 17:41:10 +0100

    contact me form

--- src/app/routes/contact/contact.component.ts
+++ src/app/routes/contact/contact.component.ts
@@ -1,12 +1,34 @@
-import {Component} from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
+import {FormsModule, NgForm} from "@angular/forms";
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
 
+  @ViewChild('contactForm') private contactForm!: NgForm;
+
+  constructor(private http: HttpClient) {
+  }
+
+
+  send(event: SubmitEvent) {
+    event.preventDefault();
+    this.http.post('https://vinojcic.com/contact', this.contactForm.value).subscribe(
+      (response) => {
+        console.log("SENT")
+      },
+      (error) => {
+        console.log("NOT SENT")
+
+      }
+    );
+  }
 }
