1299f2f494eb24a30943261e55edd44c81a8e31a
rechen-werk <adrian.vinojcic@gmail.com>
2025-01-06 22:45:59 +0100

    automize year calculation in footer

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-footer',
@@ -8,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
-
+  anno: number = new Date().getFullYear();
 }
