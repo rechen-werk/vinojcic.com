7a364e32a7097d729ab74ec5615d4a4cd134c4c8
adrian <adrian.vinojcic@gmail.com>
2026-03-01 12:19:48 +0100

    input form colors; allow login, remove register (will be request based, so no random strangers are in the system)

--- src/app/routes/login/login.component.ts
+++ src/app/routes/login/login.component.ts
@@ -4,12 +4,25 @@ import {NgIf} from "@angular/common";
 import {HttpClient} from "@angular/common/http";
 import {environment} from "../../../environments/environment";
 import {Router} from "@angular/router";
+import {animate, state, style, transition, trigger} from "@angular/animations";
 
 @Component({
   selector: 'app-login',
   imports: [
     FormsModule,
     NgIf
+  ],
+  animations: [
+    trigger('fadeInOut', [
+      state('void', style({ opacity: 0 })),
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('0ms', style({ opacity: 1 }))
+      ]),
+      transition(':leave', [
+        animate('1000ms', style({ opacity: 0 }))
+      ])
+    ])
   ],
   templateUrl: 'login.component.html',
   styleUrl: 'login.component.scss'
@@ -18,14 +31,11 @@ export class LoginComponent {
   @ViewChild('loginForm') private loginForm!: NgForm;
 
   protected inProgress = false;
+  protected errorMessage: string | null = null
 
   constructor(
     private http: HttpClient,
     private router: Router) {
-  }
-
-  goToRegister() {
-    this.router.navigateByUrl("/register");
   }
 
   login(event: SubmitEvent) {
@@ -39,6 +49,11 @@ export class LoginComponent {
         this.router.navigateByUrl("/dashboard");
       },
       (err) => {
+        this.errorMessage = err.error;
+        this.inProgress = false;
+        setTimeout(() => {
+          this.errorMessage = null;
+        }, 5000);
         this.inProgress = false;
       }
     );
