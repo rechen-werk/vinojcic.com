1345528f25bb3ecd071f78dc859416f6b431633f
adrian <adrian.vinojcic@gmail.com>
2026-06-20 18:57:19 +0200

    translate legal notice

--- src/app/routes/impressum/impressum.ts
+++ src/app/routes/impressum/impressum.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import {TranslatePipe} from "@ngx-translate/core";
 
 @Component({
   selector: 'app-impressum',
-  imports: [],
+  imports: [
+    TranslatePipe
+  ],
   templateUrl: './impressum.html',
   styleUrl: './impressum.scss',
 })
